3cd8b12e7aff7fdabff8a17d0a6347b0def6b7d5
Jason Cox <physics4life.jgcox@gmail.com>
2021-08-31 02:27:29 -0700

    stacks/queues done

--- dsa-stacks-queues/stack.js
+++ dsa-stacks-queues/stack.js
@@ -20,26 +20,46 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    const node = new Node(val)
+    if (this.isEmpty()) {
+      this.size = 1
+      this.first = node
+      this.last = node
+    }
+    else {
+      node.next = this.first
+      this.first = node
+      this.size++
+    }
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if (this.isEmpty()) throw new Error("Queue is empty")
+    this.size--
+    const toPop = this.first.val
+    if (this.size) {
+      this.first = this.first.next
+    }
+    else {
+      this.first = null
+      this.last = null
+    }
+    return toPop
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    return this.first.val
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return !this.first
   }
 }
 
